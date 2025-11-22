b4818395edcb57356a06be535964eb81ea0aa466
iqteshadahmad <iqteshadahmad786@gmail.com>
2025-11-22 20:43:16 +0530

    Prepare for Render deployment

--- donenv/settings.py
+++ donenv/settings.py
@@ -125,3 +125,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+ALLOWED_HOSTS = ["*", ".onrender.com"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+
