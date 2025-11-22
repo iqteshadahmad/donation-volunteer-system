d4cbb8cc8b39e9cb73ca7bd982c0992e0a0ed7fd
iqteshadahmad <iqteshadahmad786@gmail.com>
2025-11-22 21:29:28 +0530

    Update settings

--- donenv/settings.py
+++ donenv/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'donenv.urls'
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_URLS= BASE_DIR/'static'
 
 MEDIA_URL='/media/'
@@ -128,5 +129,9 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
