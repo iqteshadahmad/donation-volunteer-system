f2262a8007be88493e34a33f8a0ce961ea9ca966
iqteshadahmad <iqteshadahmad786@gmail.com>
2025-11-22 20:14:22 +0530

    Initial project upload

---
+++ app/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Donor, Volunteer, DonationArea, Donation, Gallery
+
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ('id','user','contact','address','regdate')
+
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ('id','user','contact','address','regdate')
+
+@admin.register(DonationArea)
+class DonationAreaAdmin(admin.ModelAdmin):
+    list_display = ('id','areaname','description','creationdate')
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'donor', 'get_volunteer', 'donationarea', 'donationname')
+
+    def get_volunteer(self, obj):
+        return obj.volunteer.user.get_full_name() if obj.volunteer else "Not Assigned Yet"
+    get_volunteer.short_description = "Volunteer"
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('id','donation','deliverypic','creationdate')
+
+
